89cce96c3695af7a1089e5be5e290d2657460e0f
sly <sylvester@archis.company>
2024-01-06 18:54:37 +0100

    location time and date UI implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import LocationTime from "./components/locationTime/locationTime";
 import SearchBar from "./components/searchBar/searchBar";
 
 function App() {
   return (
     <>
       <SearchBar />
+
+      <div className="app">
+        <LocationTime />
+      </div>
     </>
   );
 }
---
+++ src/components/locationTime/locationTime.jsx
@@ -0,0 +1,16 @@
+import "./locationTime.css";
+
+const LocationTime = () => {
+  return (
+    <div className="locationContainer">
+      <h2 className="locationName">Athens</h2>
+
+      <div className="locationTimeDateContainer">
+        <h1 className="locationTime">09:00</h1>
+        <p className="locationDate">Thursday, 31 Aug</p>
+      </div>
+    </div>
+  );
+};
+
+export default LocationTime;
